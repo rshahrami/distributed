3e1a545b3435877afe44341168490b358b5bde57
EhnNoz <enorozzadeh@gmail.com>
2025-06-08 11:59:28 +0330

    3rd Edition

--- chbackend/sender/urls.py
+++ chbackend/sender/urls.py
@@ -6,6 +6,8 @@ router = DefaultRouter()
 router.register(r'provinces', views.ProvinceViewSet, basename='provinces')
 router.register(r'channels', views.ChannelViewSet, basename='channels')
 router.register(r'posts', views.PostViewSet, basename='posts')
+router.register(r'category', views.CategoryViewSet, basename='category')
+router.register(r'platform-token', views.PlatformTokenViewSet, basename='platform-token')
 
 urlpatterns = [
     path('', include(router.urls)),
--- chbackend/sender/views.py
+++ chbackend/sender/views.py
@@ -1,21 +1,45 @@
 from rest_framework import viewsets
-from .models import Province, Channel, Post
-from .serializers import ProvinceSerializer, ChannelSerializer, PostSerializer
+from .models import Province, Channel, Post, Category, PlatformToken
+from .serializers import ProvinceSerializer, ChannelSerializer, PostSerializer, \
+    CategorySerializer, PlatformTokenSerializer
+from django.core.exceptions import PermissionDenied
+
 
 class ProvinceViewSet(viewsets.ModelViewSet):
     queryset = Province.objects.all()
     serializer_class = ProvinceSerializer
 
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class PlatformTokenViewSet(viewsets.ModelViewSet):
+    queryset = PlatformToken.objects.all()
+    serializer_class = PlatformTokenSerializer
+
+
 class ChannelViewSet(viewsets.ModelViewSet):
     queryset = Channel.objects.all()
     serializer_class = ChannelSerializer
+
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
     def get_queryset(self):
-        # محدود کردن دسترسی بر اساس کاربر
-        if not self.request.user.is_superuser:
-            return Post.objects.filter(created_by=self.request.user)
+        user = self.request.user
+
+        if not user.is_authenticated:
+            raise PermissionDenied("شما باید وارد حساب کاربری خود شوید.")
+
+        if not user.is_superuser:
+            return Post.objects.filter(created_by=user)
+
         return Post.objects.all()
+
+        # if not self.request.user.is_superuser:
+        #     return Post.objects.filter(created_by=self.request.user)
+        # return Post.objects.all()
